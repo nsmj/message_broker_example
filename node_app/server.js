cce1d2af42b2055acae0aabe0334496b6d86c557
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2023-02-26 09:07:01 -0300

    Send and receive messages — basic

--- node_app/server.js
+++ node_app/server.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const amqp = require("amqplib/callback_api");
 
 const PORT = 8080;
 const HOST = "0.0.0.0";
@@ -8,7 +9,65 @@ const HOST = "0.0.0.0";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ username: "Goiaba" });
+  res.json({ username: "Initial page" });
+});
+
+app.get("/send", (req, res) => {
+  amqp.connect("amqp://rabbitmq:5672", function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = "hello";
+      var msg = "Hello world";
+
+      channel.assertQueue(queue, {
+        durable: true,
+      });
+
+      channel.sendToQueue(queue, Buffer.from(msg), {
+        persistent: true,
+      });
+      console.log(" [x] Sent %s", msg);
+    });
+  });
+
+  res.send("Finished");
+});
+
+app.get("/receive", (req, res) => {
+  amqp.connect("amqp://rabbitmq:5672", function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = "hello";
+
+      channel.assertQueue(queue, {
+        durable: true,
+      });
+
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        queue
+      );
+      channel.consume(
+        queue,
+        (msg) => {
+          console.log(" [x] Received %s", msg.content.toString());
+        },
+        {
+          noAck: true,
+        }
+      );
+    });
+  });
 });
 
 app.listen(PORT, HOST);
