7196bcf7a39952e47202c2e9ef3ba1d3f12756fa
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2023-02-26 09:30:19 -0300

    Put the consumer in a separate file

--- node_app/server.js
+++ node_app/server.js
@@ -38,36 +38,4 @@ app.get("/send", (req, res) => {
   res.send("Finished");
 });
 
-app.get("/receive", (req, res) => {
-  amqp.connect("amqp://rabbitmq:5672", function (error0, connection) {
-    if (error0) {
-      throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-      if (error1) {
-        throw error1;
-      }
-      var queue = "hello";
-
-      channel.assertQueue(queue, {
-        durable: true,
-      });
-
-      console.log(
-        " [*] Waiting for messages in %s. To exit press CTRL+C",
-        queue
-      );
-      channel.consume(
-        queue,
-        (msg) => {
-          console.log(" [x] Received %s", msg.content.toString());
-        },
-        {
-          noAck: true,
-        }
-      );
-    });
-  });
-});
-
 app.listen(PORT, HOST);
